d909abdb05658170d46813c866e39764d0cd1e50
sergey <sergey.fomin.90@inbox.ru>
2024-07-05 22:12:23 +0400

    Initial commit4

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton('Открыть веб страницу',
-                                          web_app=WebAppInfo(url='https://github.com/sergey5478/Telegram/blob/main/index.html')))
+                                          web_app=WebAppInfo(url='https://sergey5478.github.io/Telegram/')))
     await message.reply('Hello', reply_markup=markup)
 
 
