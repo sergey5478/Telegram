10e456fa8d6ce23e3285566a3383be0f405d42d9
sergey <sergey.fomin.90@inbox.ru>
2024-07-08 21:46:54 +0400

    rt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 bot = Bot('7451812537:AAHOHpRCSwdV6bpvt1uVYbU1o2xdEwBb0eI')
 dp = Dispatcher(bot)
@@ -7,10 +8,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Открыть веб страницу',
+    markup = types.ReplyKeyboardMarkup()#InlineKeyboardMarkup
+    markup.add(types.KeyboardButton('Открыть веб страницу',
                                           web_app=WebAppInfo(url='https://sergey5478.github.io/Telegram/')))
-    await message.reply('Hello', reply_markup=markup)
+    await message.answer('Hello', reply_markup=markup)
 
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'{res["name"]}, {res["email"]}')
 
 executor.start_polling(dp)
